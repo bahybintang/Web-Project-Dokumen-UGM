176c93d5b0f8ac4e2eacbfb2e95636fa8e65c828
bahybintang <bahybintang@gmail.com>
2018-10-16 14:08:34 +0700

    commit

---
+++ models/database.js
@@ -0,0 +1,101 @@
+var mongoose = require('mongoose');
+
+const dbSchema = mongoose.Schema({
+    file_name: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    fakultas: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true
+    }
+});
+dbSchema.index({file_name:'text', fakultas:'text', title:'text'});
+
+const database = module.exports = mongoose.model('database', dbSchema);
+
+module.exports.getData = function(callback, limit){
+    database.find(callback).limit(limit);
+}
+
+module.exports.addData = function(data, callback){
+    database.create(data, callback);
+}
+
+module.exports.getDataById = function(id, callback){
+    var query = {'_id': id};
+    database.findById(query, callback);
+}
+
+module.exports.deleteData = function(id, callback){
+    var query = {'_id': id};
+    database.remove(query, callback);
+}
+
+module.exports.updateData = function(id, data, options, callback){
+    var query = {'_id': id};
+    var update = {
+        file_name: data.file_name,
+        url: data.url,
+        fakultas: data.fakultas,
+        title: data.title
+    };
+    database.findByIdAndUpdate(query, update, options, callback);
+}
+
+module.exports.getPage = function(req, callback){
+    var _page = req.query._page;
+    var _page_len = req.query._page_len;
+    database.find(callback).skip(_page_len*(_page-1)).limit(_page_len-0);
+}
+
+module.exports.searchData = function(req, callback, limit){
+    var key = new RegExp(req.query.key, "i");
+    console.log(key);
+    var query = { $or: [{
+        fakultas: {
+            $regex: key
+        }},
+        {file_name: {
+            $regex: key
+        }},
+        {title: {
+            $regex: key
+        }}]
+    };
+
+    if(req.query._page === null && req.query._page_len === null){
+        database.find(query, callback).limit(limit);
+    }
+    else{
+        var _page = req.query._page;
+        var _page_len = req.query._page_len;
+        database.find(query, callback).skip(_page_len*(_page-1)).limit(_page_len-0);
+    }
+    
+}
+
+// OLD SEARCH
+// module.exports.searchData = function(key, callback, limit){
+//     key = new RegExp(key, "i");
+//     console.log(key);
+//     var query = { $or: [{
+//         fakultas: {
+//             $regex: key
+//         }},
+//         {file_name: {
+//             $regex: key
+//         }},
+//         {title: {
+//             $regex: key
+//         }}]
+//     };
+//     database.find(query, callback).limit(limit);
+// }
---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+DB = require('./models/database');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://bintang-db:Password1@ds047207.mlab.com:47207/project-one');
+
+var db = mongoose.connection;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+    res.send("Pake /api gan!");
+});
+
+app.post('/api/add', (req, res) => {
+    DB.addData(req.body, function(err){
+        if(err){
+            throw err;
+        }
+        else{
+            console.log("data added");
+            res.status(200).end();
+        }
+    });
+});
+
+app.delete('/api/delete/:id', (req, res) => {
+    DB.deleteData(req.params.id, function(err){
+        if(err){
+            throw err;
+            
+        }
+        else{
+            console.log("data deleted");
+            res.status(200).end();
+        }
+    });
+});
+
+app.get('/api/get', (req, res) => {
+    DB.getData(function(err, data){
+        if(err){
+            throw err;
+            
+        }
+        res.json(data);
+    });
+});
+
+app.get('/api/get/:id', (req, res) => {
+    DB.getDataById(req.params.id, function(err, data){
+        if(err){
+            throw err;
+            
+        }
+        res.json(data);
+    });
+});
+
+app.post('/api/update/:id', (req, res) => {
+    DB.updateData(req.params.id, req.body, {}, function(err){
+        if(err){
+            throw err;
+            
+        }
+        else{
+            console.log("data " + req.params.id + " updated!");
+        }
+    });
+});
+
+// search/?key=val&_page=val&_page_len=val
+// _page dan _page_len optional
+app.get('/api/search', (req, res) => {
+    DB.searchData(req, function(err, data){
+        if(err){
+            throw err;
+        }
+        res.json(data);
+    });
+});
+
+// /page/_page=val&_page_len=val
+app.get('/api/page', (req, res) => {
+    DB.getPage(req, function(err, data){
+        if(err){
+            throw err;
+        }
+        res.json(data);
+    });
+});
+
+app.listen(3000, function(){
+    console.log("listening port 3000...")
+});
