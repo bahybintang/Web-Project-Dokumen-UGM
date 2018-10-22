8068e3a414180fe07f2223b3e8d9ae77e79f2c07
bahybintang <bahybintang@gmail.com>
2018-10-22 20:44:38 +0700

    frontend edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import './App.css';
-
 class App extends Component {
   state = {
     response: []
@@ -21,29 +19,56 @@ class App extends Component {
     return body;
   };
 
+  iconGenerate = (s) => {
+    if(s === 'pdf'){
+      return <img src={"/assets/images/pdf-icon.png"} style={{width:"20px", height:"20px"}} alt="pdf"/>;
+    }
+    else if(s === 'doc' || s === 'docx'){
+      return <img src={"/assets/images/doc-icon.png"} style={{width:"20px", height:"20px"}} alt="doc"/>;
+    }
+    else if(s === 'xls' || s === 'xlsx'){
+      return <img src={"/assets/images/xls-icon.png"} style={{width:"20px", height:"20px"}} alt="xls"/>;
+    }
+    else{
+      return <img src={"/assets/images/other-icon.png"} style={{width:"20px", height:"20px"}} alt="other"/>;
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <table>
-          <tr>
-            <th>Fakultas</th>
-            <th>Title</th>
-            <th>Download</th>
-          </tr>
+      <div className="table-responsive">
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th style={{width:"10%"}}>Fakultas</th>
+              <th style={{width:"40%"}}>Title</th>
+              <th className="text-center" style={{width:"10%"}}>File Type</th>
+              <th className="text-center" style={{width:"20%"}}>Download</th>
+            </tr>
+          </thead>
+          <tbody>
             {
             this.state.response.map(item => {
               return (
                 <tr>
-                  <td>{item.fakultas}</td>
+                  <td>{item.fakultas.toUpperCase()}</td>
                   <td>{item.title}</td>
-                  <td><a href={item.url}>download</a></td>
+                  <td className="text-center">{this.iconGenerate(item.file_name.split('.')[item.title.split('.').length])}</td>
+                  <td className="text-center"><a href={item.url}><img src={"/assets/images/download.png"} style={{width:"20px", height:"20px"}} alt="download-button"/></a></td>
                 </tr>)
             })
             }
-          </table>
+          </tbody>
+        </table>
       </div>
     );
   }
+}
+
+String.prototype.toProperCase = function()
+{
+  return this.toLowerCase().replace(/^(.)|\s(.)/g, 
+      function($1) { return $1.toUpperCase(); });
 }
 
 export default App;
